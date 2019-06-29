1a6b70a2ecf140971b9d396ade2b9101f53a5c79
Gustavo Rosolem <guspace92@hotmail.com>
2019-06-29 13:15:07 -0300

    Redirect to GC robots

--- node/middlewares/robots.ts
+++ node/middlewares/robots.ts
@@ -1,8 +1,11 @@
+import { Functions } from '@gocommerce/utils'
+
 const TEN_MINUTES_S = 10 * 60
 
 export const robots = async (ctx: Context) => {
   const {vtex: {account, production}} = ctx
-  const {clients: {robots: robotsDataSource}} = ctx
+  const { clients } = ctx
+  const robotsDataSource = Functions.isGoCommerceAcc(account) ? clients.robotsGC : clients.robots
   const data = await robotsDataSource.fromLegacy(account)
   ctx.set('Content-Type', 'text/plain')
   ctx.body = data
