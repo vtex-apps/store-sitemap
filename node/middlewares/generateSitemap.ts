3c283d54bee3d73e0819c52598dc082559e26d13
Victor Raposo <vwraposo@gmail.com>
2020-03-23 10:09:57 -0300

    Adds new sitempap logic

---
+++ node/middlewares/generateSitemap.ts
@@ -0,0 +1,54 @@
+import { startsWith } from "ramda";
+
+/* eslint-disable no-await-in-loop */
+export const SITEMAP_BUCKET = "_SITEMAP_";
+export const SITEMAP_INDEX = "sitemap_index";
+export const GENERATE_SITEMAP_EVENT = "sitemap.generate";
+const LIST_LIMIT = 500;
+
+const currentDate = (): string => new Date().toISOString().split("T")[0];
+
+const generate = async (ctx: Context) => {
+  const { vbase, rewriter } = ctx.clients;
+  let response;
+  let from = 0;
+  let next: Maybe<string>;
+  await vbase.saveJSON(SITEMAP_BUCKET, SITEMAP_INDEX, { index: [] });
+  do {
+    response = await rewriter.listInternals(LIST_LIMIT, next);
+    const length: number = response.routes?.length;
+    if (!response.routes || !length) {
+      next = response.next;
+      continue;
+    }
+    const list = response.routes.filter(
+      internal =>
+        !startsWith("notFound", internal.type) && internal.id !== "search"
+    );
+
+    next = response.next;
+
+    const to = from + length;
+    const entry = `sitemap-${from}-${to}`;
+    const index = await vbase.getJSON<string[]>(
+      SITEMAP_BUCKET,
+      SITEMAP_INDEX,
+      true
+    );
+    index.push(entry);
+    await Promise.all([
+      vbase.saveJSON(SITEMAP_BUCKET, SITEMAP_INDEX, {
+        index,
+        lastUpdated: currentDate()
+      }),
+      vbase.saveJSON(SITEMAP_BUCKET, entry, list)
+    ]);
+    from += length;
+  } while (next);
+};
+
+export async function generateSitemap(ctx: Context) {
+  generate(ctx);
+
+  ctx.status = 200;
+}
--- node/middlewares/newSitemap.ts
+++ node/middlewares/newSitemap.ts
@@ -1,36 +1,38 @@
-import * as cheerio from 'cheerio'
-import { keys, map, mergeAll, prop, range, replace } from 'ramda'
+import { VBase } from "@vtex/api";
+import * as cheerio from "cheerio";
+import { replace } from "ramda";
+import { Internal } from "vtex.rewriter";
 
-import { Internal, RouteIndexFileEntry } from '../clients/rewriter'
-import { currentDate } from '../resources/utils'
+import {
+  GENERATE_SITEMAP_EVENT,
+  SITEMAP_BUCKET,
+  SITEMAP_INDEX
+} from "./generateSitemap";
 
-const MAX_ROUTES_PER_REQUEST = 100
-const TEN_MINUTES_S = 10 * 60
+const ONE_DAY_S = 24 * 60 * 60;
 
-const sitemapIndexEntry = (forwardedHost: string, rootPath: string, entity: string, firstIndex?: number, lastIndex?: number): string => {
-  if (firstIndex && lastIndex) {
-    return `
-      <sitemap>
-        <loc>https://${forwardedHost}${rootPath}/_v/public/newsitemap/${entity}-${firstIndex}-${lastIndex}.xml</loc>
-        <lastmod>${currentDate()}</lastmod>
-      </sitemap>
-    `
-  }
-  return `
-    <sitemap>
-      <loc>https://${forwardedHost}${rootPath}/_v/public/newsitemap/${entity}.xml</loc>
-      <lastmod>${currentDate()}</lastmod>
-    </sitemap>
-  `
-}
+const sitemapIndexEntry = (
+  forwardedHost: string,
+  rootPath: string,
+  entry: string,
+  lastUpdated: string
+) =>
+  `<sitemap>
+      <loc>https://${forwardedHost}${rootPath}/_v/public/newsitemap/${entry}.xml</loc>
+      <lastmod>${lastUpdated}</lastmod>
+    </sitemap>`;
 
-const URLEntry = (forwardedHost: string, rootPath: string, route: Internal): string => {
+const URLEntry = (
+  forwardedHost: string,
+  rootPath: string,
+  route: Internal
+): string => {
   let entry = `
       <loc>https://${forwardedHost}${rootPath}${route.from}</loc>
       <lastmod>${currentDate()}</lastmod>
       <changefreq>weekly</changefreq>
       <priority>0.4</priority>
-    `
+    `;
   if (route.imagePath && route.imageTitle) {
     // add image metainfo
     entry = `
@@ -38,52 +40,92 @@ const URLEntry = (forwardedHost: string, rootPath: string, route: Internal): str
       <image:loc>${route.imagePath}</image:loc>
       <image:title>${route.imageTitle}</image:title>
     </image:image>
-    ` + entry
+    ${entry}`;
   }
-  return `<url>${entry}</url>`
-}
+  return `<url>${entry}</url>`;
+};
 
-export async function sitemap (ctx: Context) {
-  const { vtex: { production }, clients: { rewriter } } = ctx
-  const forwardedHost = ctx.get('x-forwarded-host')
-  let rootPath = ctx.get('x-vtex-root-path')
+const sitemapIndex = async (
+  forwardedHost: string,
+  rootPath: string,
+  vbase: VBase
+) => {
+  const $ = cheerio.load(
+    '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    {
+      xmlMode: true
+    }
+  );
+
+  const indexData = await vbase.getJSON<{
+    index: string[];
+    lastUpdated: string;
+  }>(SITEMAP_BUCKET, SITEMAP_INDEX, true);
+
+  if (!indexData) {
+    throw new Error(
+      "Sitemap not generated, you need to do that first <LINK TO DOC>"
+    );
+  }
+  const { index, lastUpdated } = indexData;
+  index.forEach(entry =>
+    $("sitemapindex").append(
+      sitemapIndexEntry(forwardedHost, rootPath, entry, lastUpdated)
+    )
+  );
+  return $;
+};
+
+export async function sitemap(ctx: Context) {
+  const {
+    vtex: { production },
+    clients: { vbase, events }
+  } = ctx;
+  const forwardedHost = ctx.get("x-forwarded-host");
+  let rootPath = ctx.get("x-vtex-root-path");
   // Defend against malformed root path. It should always start with `/`.
-  if (rootPath && !rootPath.startsWith('/')) {
-    rootPath = `/${rootPath}`
+  if (rootPath && !rootPath.startsWith("/")) {
+    rootPath = `/${rootPath}`;
   }
-  const [forwardedPath] = ctx.get('x-forwarded-path').split('?')
+  const [forwardedPath] = ctx.get("x-forwarded-path").split("?");
 
-  const indexArray = await rewriter.routesIndexFiles().then(prop('routeIndexFiles'))
-  const indexTable = mergeAll(map((obj: RouteIndexFileEntry) => ({ [obj.fileName]: obj.fileSize }), indexArray))
-  let $: any
-  if (forwardedPath === '/_v/public/newsitemap/sitemap.xml') {
-    $ = cheerio.load('<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">', {
-      xmlMode: true,
-    })
-    const indexTitles = keys(indexTable) as string[]
-    indexTitles.forEach((entity: string) => {
-      const indexSize = indexTable[entity]
-      if (indexSize <= MAX_ROUTES_PER_REQUEST) {
-        $('sitemapindex').append(sitemapIndexEntry(forwardedHost, rootPath, entity))
-      } else {
-        range(0, Math.floor(indexSize/MAX_ROUTES_PER_REQUEST)).forEach(x => {
-          const [firstIndex, lastIndex] = [x*MAX_ROUTES_PER_REQUEST, (x+1)*MAX_ROUTES_PER_REQUEST-1]
-          $('sitemapindex').append(sitemapIndexEntry(forwardedHost, rootPath, entity, firstIndex, lastIndex))
-        })
-      }
-    })
+  let $: any;
+  if (forwardedPath === "/_v/public/newsitemap/sitemap.xml") {
+    $ = await sitemapIndex(forwardedHost, rootPath, vbase);
   } else {
-    $ = cheerio.load('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">', {
-      xmlMode: true,
-    })
-    const [entity, startIndex=0, maybeLastIndex] = replace(/^\/_v\/public\/newsitemap\/(.+?)\.xml$/, '$1', forwardedPath).split('-')
-    const lastIndex = maybeLastIndex ? Number(maybeLastIndex) : Number(indexTable[entity]) - 1
-    const routes = await rewriter.listInternals(Number(startIndex), lastIndex as number, entity)
-    routes.forEach((route: Internal) => $('urlset').append(URLEntry(forwardedHost, rootPath, route)))
+    $ = cheerio.load(
+      '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">',
+      {
+        xmlMode: true
+      }
+    );
+    const fileName = replace(
+      /^\/_v\/public\/newsitemap\/(.+?)\.xml$/,
+      "$1",
+      forwardedPath
+    );
+    const maybeRoutes = await vbase.getJSON<Internal[]>(
+      SITEMAP_BUCKET,
+      fileName,
+      true
+    );
+    if (!maybeRoutes) {
+      ctx.status = 404;
+      throw new Error("Sitemap entry not found");
+    }
+    maybeRoutes.forEach((route: Internal) => {
+      $("urlset").append(URLEntry(forwardedHost, rootPath, route));
+    });
   }
 
-  ctx.set('Content-Type', 'text/xml')
-  ctx.body = $.xml()
-  ctx.status = 200
-  ctx.set('cache-control', production ? `public, max-age=${TEN_MINUTES_S}`: 'no-cache')
+  ctx.set("Content-Type", "text/xml");
+  ctx.body = $.xml();
+  ctx.status = 200;
+  ctx.set(
+    "cache-control",
+    production ? `public, max-age=${ONE_DAY_S}` : "no-cache"
+  );
+  if (production) {
+    events.sendEvent("", GENERATE_SITEMAP_EVENT);
+  }
 }
