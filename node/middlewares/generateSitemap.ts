d7a5f1e02f9d240f1dfd904452830e47e8b7827f
Victor Raposo <vwraposo@gmail.com>
2020-03-23 10:17:25 -0300

    Removes semicolon

--- node/middlewares/generateSitemap.ts
+++ node/middlewares/generateSitemap.ts
@@ -1,54 +1,54 @@
-import { startsWith } from "ramda";
+import { startsWith } from 'ramda'
 
 /* eslint-disable no-await-in-loop */
-export const SITEMAP_BUCKET = "_SITEMAP_";
-export const SITEMAP_INDEX = "sitemap_index";
-export const GENERATE_SITEMAP_EVENT = "sitemap.generate";
-const LIST_LIMIT = 500;
+export const SITEMAP_BUCKET = '_SITEMAP_'
+export const SITEMAP_INDEX = 'sitemap_index'
+export const GENERATE_SITEMAP_EVENT = 'sitemap.generate'
+const LIST_LIMIT = 500
 
-const currentDate = (): string => new Date().toISOString().split("T")[0];
+const currentDate = (): string => new Date().toISOString().split('T')[0]
 
 const generate = async (ctx: Context) => {
-  const { vbase, rewriter } = ctx.clients;
-  let response;
-  let from = 0;
-  let next: Maybe<string>;
-  await vbase.saveJSON(SITEMAP_BUCKET, SITEMAP_INDEX, { index: [] });
+  const { vbase, rewriter } = ctx.clients
+  let response
+  let from = 0
+  let next: Maybe<string>
+  await vbase.saveJSON(SITEMAP_BUCKET, SITEMAP_INDEX, { index: [] })
   do {
-    response = await rewriter.listInternals(LIST_LIMIT, next);
-    const length: number = response.routes?.length;
+    response = await rewriter.listInternals(LIST_LIMIT, next)
+    const length: number = response.routes?.length
     if (!response.routes || !length) {
-      next = response.next;
-      continue;
+      next = response.next
+      continue
     }
     const list = response.routes.filter(
       internal =>
-        !startsWith("notFound", internal.type) && internal.id !== "search"
-    );
+        !startsWith('notFound', internal.type) && internal.id !== 'search'
+    )
 
-    next = response.next;
+    next = response.next
 
-    const to = from + length;
-    const entry = `sitemap-${from}-${to}`;
+    const to = from + length
+    const entry = `sitemap-${from}-${to}`
     const index = await vbase.getJSON<string[]>(
       SITEMAP_BUCKET,
       SITEMAP_INDEX,
       true
-    );
-    index.push(entry);
+    )
+    index.push(entry)
     await Promise.all([
       vbase.saveJSON(SITEMAP_BUCKET, SITEMAP_INDEX, {
         index,
-        lastUpdated: currentDate()
+        lastUpdated: currentDate(),
       }),
-      vbase.saveJSON(SITEMAP_BUCKET, entry, list)
-    ]);
-    from += length;
-  } while (next);
-};
+      vbase.saveJSON(SITEMAP_BUCKET, entry, list),
+    ])
+    from += length
+  } while (next)
+}
 
 export async function generateSitemap(ctx: Context) {
-  generate(ctx);
+  generate(ctx)
 
-  ctx.status = 200;
+  ctx.status = 200
 }
