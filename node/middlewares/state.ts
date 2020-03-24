9df4b5e333932a0a1f19a2735d1d937d1a7a5a99
Victor Raposo <vwraposo@gmail.com>
2020-03-24 10:20:14 -0300

    Fixes types

--- node/middlewares/state.ts
+++ node/middlewares/state.ts
@@ -1,7 +1,5 @@
 export async function prepareState(ctx: Context, next: () => Promise<any>) {
-  ctx.state = {
-    platform: ctx.vtex.platform || 'vtex',
-  }
+  ctx.state.platform = ctx.vtex.platform || 'vtex'
 
   await next()
 }
