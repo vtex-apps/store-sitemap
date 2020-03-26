911a99a630ed99b71863d9af1fa578ca0ebb3dcf
Joao Fidelis <joaogfidelis@gmail.com>
2020-03-26 20:55:46 -0300

    look for robots in all dependencies

--- node/middlewares/robots.ts
+++ node/middlewares/robots.ts
@@ -1,6 +1,28 @@
-import { prop, toString } from "ramda"
+import { Apps, Logger } from "@vtex/api"
 
 const TEN_MINUTES_S = 10 * 60
+
+interface RobotsFile {
+  robots: Record<string, string>
+}
+
+const SITEMAP_BUILD_FILE = 'dist/vtex.store-sitemap/build.json'
+
+const getAppRobot = (apps: Apps, deps: Record<string, string[]>, logger: Logger) => async (appName: string) => {
+  const robots = await apps.getAppJSON(appName, SITEMAP_BUILD_FILE, true)
+  if (robots && !deps[appName].includes('vtex.store-sitemap@1.x')) {
+    logger.warn({ message: `App ${appName} exports robots config, but does not depend on vtex.store-sitemap@1.x` })
+  }
+  return robots as Partial<RobotsFile> | null
+}
+
+const getRobots = async (apps: Apps, logger: Logger) => {
+  const deps = await apps.getDependencies()
+  const depList = Object.keys(deps).filter(key => !key.startsWith('infra:'))
+  const buildFiles = await Promise.all(depList.map(getAppRobot(apps, deps, logger)))
+  const withRobots = buildFiles.filter(file => file?.robots) as RobotsFile[]
+  return withRobots.map(data => data.robots)
+}
 
 export async function robots(ctx: Context) {
   const { vtex: { account, production, platform }, clients, state: { bindingId } } = ctx
@@ -8,19 +30,13 @@ export async function robots(ctx: Context) {
   const robotsDataSource = platform === 'gocommerce' ? clients.robotsGC : clients.robots
 
   if (bindingId) {
-    const robotsConfigs =
-      await clients.apps.getAppFile(`${account}.robots-settings@0.x`, 'dist/vtex.store-sitemap/bindings.json', true)
-        .then(prop('data'))
-        .then(toString)
-        .then(JSON.parse)
-        .catch(_ => null)
-    if (robotsConfigs) {
-      const robotForBinding = robotsConfigs[bindingId]
-      if (!robotForBinding) {
-        throw Error('No robot config specified for current binding')
-      }
-      data = robotForBinding
+    const robotsList = await getRobots(clients.apps, clients.logger)
+    const robotData = robotsList.find(robotConfig => robotConfig[bindingId])
+    if (!robotData) {
+      throw Error('No robot config specified for current binding')
     }
+
+    data = robotData[bindingId]
   }
   if (!data || !bindingId) {
     data = await robotsDataSource.fromLegacy(account)
