f5e4c6a712c1744d415783dcad1aa08c0d5b789a
Joao Fidelis <joaogfidelis@gmail.com>
2020-03-27 17:48:19 -0300

    go back to only one app startegy

--- node/middlewares/robots.ts
+++ node/middlewares/robots.ts
@@ -1,4 +1,4 @@
-import { Apps, Logger } from "@vtex/api"
+import { Apps } from "@vtex/api"
 
 const TEN_MINUTES_S = 10 * 60
 
@@ -8,32 +8,17 @@ interface RobotsFile {
 
 const SITEMAP_BUILD_FILE = 'dist/vtex.store-sitemap/build.json'
 
-const getAppRobot = (apps: Apps, deps: Record<string, string[]>, logger: Logger) => async (appName: string) => {
-  const robots = await apps.getAppJSON(appName, SITEMAP_BUILD_FILE, true)
-  if (robots && !deps[appName].includes('vtex.store-sitemap@1.x')) {
-    logger.warn({ message: `App ${appName} exports robots config, but does not depend on vtex.store-sitemap@1.x` })
-  }
-  return robots as Partial<RobotsFile> | null
-}
-
-const getRobots = async (apps: Apps, logger: Logger) => {
-  const deps = await apps.getDependencies()
-  const depList = Object.keys(deps).filter(key => !key.startsWith('infra:'))
-  const buildFiles = await Promise.all(depList.map(getAppRobot(apps, deps, logger)))
-  const withRobots = buildFiles.filter(file => file?.robots) as RobotsFile[]
-  return withRobots.map(data => data.robots)
+const getRobotForBinding = async (bindingId: string, account: string, apps: Apps) => {
+  const buildFile = await apps.getAppJSON<Partial<RobotsFile> | null>(`${account}.robots@0.x`, SITEMAP_BUILD_FILE, true)
+  return buildFile?.robots?.[bindingId]
 }
 
 export async function robots(ctx: Context) {
   const { vtex: { account, production, platform }, clients, state: { bindingId } } = ctx
-  let data = ''
+  let data: string | undefined = undefined
   const robotsDataSource = platform === 'gocommerce' ? clients.robotsGC : clients.robots
   if (bindingId) {
-    const robotsList = await getRobots(clients.apps, clients.logger)
-    const robotData = robotsList.find(robotConfig => robotConfig[bindingId])
-    if (robotData) {
-      data = robotData[bindingId]
-    }
+    data = await getRobotForBinding(bindingId, account, clients.apps)
   }
   if (!data || !bindingId) {
     data = await robotsDataSource.fromLegacy(account)
