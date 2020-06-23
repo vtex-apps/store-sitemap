849b5c9f409a89744582b7c1e21c34c35b4c0168
Victor Raposo <vwraposo@gmail.com>
2020-06-23 09:43:58 -0300

    Settings middleware

---
+++ node/middlewares/settings.ts
@@ -0,0 +1,48 @@
+import { appIdToAppAtMajor } from '@vtex/api'
+import { PRODUCT_ROUTES_INDEX, REWRITER_ROUTES_INDEX } from './generateMiddlewares/utils'
+
+export interface Settings {
+  enableProductRoutes: boolean
+  rewriterProductRoutes: boolean
+}
+
+const VTEX_APP_ID = process.env.VTEX_APP_ID!
+const VTEX_APP_AT_MAJOR = appIdToAppAtMajor(VTEX_APP_ID)
+
+const DEFAULT_SETTINGS = {
+  enableProductRoutes: true,
+  rewriterProductRoutes: true,
+}
+
+const INDEX_MAP = {
+  enableProductRoutes: PRODUCT_ROUTES_INDEX,
+  rewriterProductRoutes: REWRITER_ROUTES_INDEX,
+}
+
+export async function settings(ctx: Context | EventContext, next: () => Promise<void>) {
+  const {
+    clients: { apps },
+  } = ctx
+
+  const appSettings: Settings = {
+    ...DEFAULT_SETTINGS,
+    ...(await apps.getAppSettings(VTEX_APP_AT_MAJOR)),
+  }
+  const keys = Object.keys(appSettings) as Array<keyof Settings>
+  const enabledIndexFiles = keys.reduce(
+     (acc, key ) => {
+       if (appSettings[key]) {
+         acc.push(INDEX_MAP[key])
+       }
+       return acc
+     }, [] as string[])
+
+  ctx.state = {
+    ...ctx.state,
+    enabledIndexFiles,
+    settings: appSettings,
+  }
+
+
+  await next()
+}
