62c668e376af1dcde98895e3c7646e1efa1c0f3c
Victor Raposo <vwraposo@gmail.com>
2020-07-01 16:31:44 -0300

    Adds logs

--- node/middlewares/generateMiddlewares/utils.ts
+++ node/middlewares/generateMiddlewares/utils.ts
@@ -17,6 +17,8 @@ export const GENERATE_PRODUCT_ROUTES_EVENT = 'sitemap.generate:product-routes'
 export const GENERATE_APPS_ROUTES_EVENT = 'sitemap.generate:apps-routes'
 export const GROUP_ENTRIES_EVENT = 'sitemap.generate:group-entries'
 
+export const SITEMAP_GENERATION_ENABLED = false
+
 
 export const DEFAULT_CONFIG: Config = {
   generationPrefix: `${LINKED ? 'L' : ''}B`,
