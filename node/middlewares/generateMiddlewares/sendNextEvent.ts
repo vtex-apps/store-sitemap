a3a85c277c880df08049ce6c2049a9ed3b0b0852
Victor Raposo <vwraposo@gmail.com>
2020-07-06 08:58:17 -0300

    Calls wait for every event in the generation

--- node/middlewares/generateMiddlewares/sendNextEvent.ts
+++ node/middlewares/generateMiddlewares/sendNextEvent.ts
@@ -1,9 +1,14 @@
+import { WAIT_EVENT } from '../wait'
 import { sleep } from './utils'
 
 export async function sendNextEvent(ctx: EventContext) {
   const { clients: { events }, state: { nextEvent } } = ctx
   const { payload, event } = nextEvent
-  await sleep(300)
-  events.sendEvent('', event, payload)
+  await sleep(100)
+  const waitPayload: WaitEvent = {
+    event,
+    payload,
+  }
+  events.sendEvent('', WAIT_EVENT, waitPayload)
 }
 
