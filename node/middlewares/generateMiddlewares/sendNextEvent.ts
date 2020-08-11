eb24d931412429c89ab5cfbfa0e49631a4b68b2a
Victor Raposo <vwraposo@gmail.com>
2020-08-11 14:39:28 -0300

    Removes sleeps

--- node/middlewares/generateMiddlewares/sendNextEvent.ts
+++ node/middlewares/generateMiddlewares/sendNextEvent.ts
@@ -3,7 +3,7 @@ import { sleep } from './utils'
 export async function sendNextEvent(ctx: EventContext) {
   const { clients: { events }, state: { nextEvent } } = ctx
   const { payload, event } = nextEvent
-  const timeToSleep = Math.ceil(Math.random() * 500)
+  const timeToSleep = Math.ceil(Math.random() * 100)
   await sleep(timeToSleep)
   events.sendEvent('', event, payload)
 }
