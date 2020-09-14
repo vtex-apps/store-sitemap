86d8644bc057c1e55d855efb4ddee50fab3768f3
Victor Raposo <vwraposo@gmail.com>
2020-09-14 08:42:01 -0300

    Re-enables automatic sitemap generation but only for 20% of the times

--- node/middlewares/prepare.ts
+++ node/middlewares/prepare.ts
@@ -1,7 +1,7 @@
 import { parse } from 'query-string'
 
 import { BindingResolver } from '../resources/bindings'
-import { CONFIG_BUCKET, CONFIG_FILE, getBucket, getMatchingBindings, hashString } from '../utils'
+import { CONFIG_BUCKET, CONFIG_FILE, getBucket, getMatchingBindings, hashString, startSitemapGeneration } from '../utils'
 import { DEFAULT_CONFIG } from './generateMiddlewares/utils'
 
 const ONE_DAY_S = 24 * 60 * 60
@@ -53,10 +53,9 @@ export async function prepare(ctx: Context, next: () => Promise<void>) {
     'cache-control',
     production ? `public, max-age=${ONE_DAY_S}` : 'no-cache'
   )
-  // TODO: Re-enable generation
-  // if (production) {
-  //   startSitemapGeneration(ctx)
-  // }
+  if (production && Math.random() < 0.2) {
+    startSitemapGeneration(ctx)
+  }
 
   return
 }
