6991d09884f2786475e9c7e5c2fed0d60afc7566
Vinícius Seixas <vinicius.seixas@vtex.com>
2024-08-20 15:06:30 -0300

    add vbase response to `Invalid generation id` error

--- node/middlewares/generateMiddlewares/prepare.ts
+++ node/middlewares/generateMiddlewares/prepare.ts
@@ -1,16 +1,32 @@
 import { CONFIG_BUCKET, GENERATION_CONFIG_FILE } from '../../utils'
 
 export async function prepare(ctx: EventContext, next: () => Promise<void>) {
-  const { body, vtex: { logger }, clients: { vbase } } = ctx
+  const {
+    body,
+    vtex: { logger },
+    clients: { vbase },
+  } = ctx
   const { generationId } = body
+
   if (!generationId) {
     logger.error({ message: 'Missing generation id', payload: body })
     return
   }
-  const { generationId: currentGenerationId } = await vbase.getJSON<GenerationConfig>(CONFIG_BUCKET, GENERATION_CONFIG_FILE, true)
-    || { generationId: null }
+
+  const generationConfig = (await vbase.getJSON<GenerationConfig>(
+    CONFIG_BUCKET,
+    GENERATION_CONFIG_FILE,
+    true
+  )) || { generationId: null }
+
+  const { generationId: currentGenerationId } = generationConfig
+
   if (generationId !== currentGenerationId) {
-    logger.debug({ message: 'Invalid generation id', payload: body })
+    logger.debug({
+      message: 'Invalid generation id',
+      payload: body,
+      generationConfig,
+    })
     return
   }
   await next()
