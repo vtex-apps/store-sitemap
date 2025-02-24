4289a8f6141224f9565450fd3ec7da7607f2f90c
Victor Moura <victor.moura@vtex.com>
2025-02-24 10:49:35 -0300

    chore: isCrossBorder lint fix

--- node/middlewares/generateMiddlewares/isCrossBorder.ts
+++ node/middlewares/generateMiddlewares/isCrossBorder.ts
@@ -1,13 +1,18 @@
-export async function isCrossBorder(ctx: Context | EventContext, next: () => Promise<void>) {
+export async function isCrossBorder(
+  ctx: Context | EventContext,
+  next: () => Promise<void>
+) {
   const {
     clients: { tenant },
     state,
   } = ctx
 
   const { bindings } = await tenant.info()
-  const storefrontsBindings = bindings.filter(({targetProduct}) => targetProduct !== 'vtex-admin')
+  const storefrontsBindings = bindings.filter(
+    ({ targetProduct }) => targetProduct !== 'vtex-admin'
+  )
 
   state.isCrossBorder = storefrontsBindings?.length > 1
 
-  await next();
+  await next()
 }
