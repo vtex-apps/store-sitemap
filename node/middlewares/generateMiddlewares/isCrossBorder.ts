39c38c1abf515ee5a02eaa5aed0cd35ec0efeb57
Victor Moura <victor.moura@vtex.com>
2025-02-24 13:24:49 -0300

    chore: avoid reimplementation of function to get store bindings

--- node/middlewares/generateMiddlewares/isCrossBorder.ts
+++ node/middlewares/generateMiddlewares/isCrossBorder.ts
@@ -1,3 +1,5 @@
+import { getStoreBindings } from '../../utils'
+
 export async function isCrossBorder(
   ctx: Context | EventContext,
   next: () => Promise<void>
@@ -7,11 +9,7 @@ export async function isCrossBorder(
     state,
   } = ctx
 
-  const { bindings } = await tenant.info()
-  const storefrontsBindings = bindings.filter(
-    ({ targetProduct }) => targetProduct !== 'vtex-admin'
-  )
-
+  const storefrontsBindings = await getStoreBindings(tenant)
   state.isCrossBorder = storefrontsBindings?.length > 1
 
   await next()
